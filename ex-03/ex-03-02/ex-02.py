20ec1fb412d3d00d95ef4fb7101fb970d64db61b
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2022-12-05 20:51:59 +0000

    Complete ex-02 for exercise 3

---
+++ ex-03/ex-03-02/ex-02.py
@@ -0,0 +1,21 @@
+# create variables and ask user for input.
+hrs = input("Enter your hours: ")
+pay = input("Enter your pay: ")
+
+# Try function to check inputs are numeric. Exception returns error message.
+try:
+    fhrs = float(hrs)
+    fpay = float(pay)
+except:
+    print("Please enter a numeric value.")
+    exit()
+
+# if statement to recognise when overtime has been accured and adjusts formula to account for that in end pay.
+if fhrs > 40:
+    over = fhrs - 40
+    gross = (fpay * fhrs) + (1.5 * fpay * over)
+else:
+    gross = fpay * fhrs 
+
+# Print result.
+print(gross)
