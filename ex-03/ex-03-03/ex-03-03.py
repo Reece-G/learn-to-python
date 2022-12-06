b5d931405b238d5f0e1a73b311e5872dd5d53561
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2022-12-06 19:30:06 +0000

    Complete ex-03 for lesson 3

---
+++ ex-03/ex-03-03/ex-03-03.py
@@ -0,0 +1,20 @@
+# Ask user for score input.
+inp = input("Enter a score: ")
+
+# Check the input as a numerical value.
+try:
+    finp = float(inp)
+except:
+    print("Error, please enter a score.")
+    
+# Check the score inp against stored values and print result.
+if finp >= 0.9:
+    print("A")
+elif finp >= 0.8:
+    print("B")
+elif finp >= 0.7:
+    print("C")
+elif finp >= 0.6:
+    print("D")
+else:
+    print("F")  
